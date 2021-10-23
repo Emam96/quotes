93788f81a042c2430af1b68d2a286966372697e5
Emam96 <emamsh990@gmail.com>
2021-10-23 17:04:53 +0300

    test

--- app/src/main/java/quotes/Quote.java
+++ app/src/main/java/quotes/Quote.java
@@ -1,16 +1,44 @@
 package quotes;
 
 
-import java.io.Reader;
+
 
 public class Quote {
 
 
    private String author;
     private   String text;
+    private String quoteAuthor;
+
+    public String getQuoteAuthor() {
+        return quoteAuthor;
+    }
+
+    public void setQuoteAuthor(String quoteAuthor) {
+        this.quoteAuthor = quoteAuthor;
+    }
+
+    public String getQuoteText() {
+        return quoteText;
+    }
+
+    public void setQuoteText(String quoteText) {
+        this.quoteText = quoteText;
+    }
+
+    private   String quoteText;
 
 
-    public Quote(  String author, String text) {
+    public Quote( String author1, String text1) {
+
+        this.quoteAuthor = author1;
+        this.quoteText = text1;
+    }
+
+
+
+
+    public Quote( String author, String text, String random) {
 
         this.author = author;
         this.text = text;
@@ -45,6 +73,12 @@ public class Quote {
                 "Quote:" + text;
     }
 
+
+    public String toStringTwo() {
+        return
+                "author: " + quoteAuthor + "\n" +
+                        "Quote:" + quoteText;
+    }
 
 
 }
