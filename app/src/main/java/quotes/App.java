809e1505cfe9bc11b9924268cdc485d8d1419315
Emam96 <emamsh990@gmail.com>
2021-10-23 17:07:07 +0300

    lab9 finished

--- app/src/main/java/quotes/App.java
+++ app/src/main/java/quotes/App.java
@@ -18,7 +18,7 @@ public class App {
     public static void main(String[] args) throws IOException {
 
 
-        readFromJson("app/src/main/resources/quotes.json");
+        readFromJson("http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en");
 
 
     }
@@ -26,15 +26,32 @@ public class App {
 
     public static void readFromJson( String thePath) throws IOException {
 
-        Gson gson = new Gson();
-        Reader reader = Files.newBufferedReader(Paths.get(thePath));
-        BufferedReader readJson = new BufferedReader(reader);
 
-        Quote[] result = gson.fromJson(readJson, Quote[].class);
-        int max = result.length;
-        int min = 1;
-        int randomLine = (int)Math.floor(Math.random()*(max-min+1)+min);
-        System.out.println(result[randomLine].toString());
+        try {
+            URL url = new URL(thePath);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
+
+            InputStream inputStream = connection.getInputStream();
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+            BufferedReader in = new BufferedReader(inputStreamReader);
+            String line = in.readLine();
+            while(line != null){
+                System.out.println(line);
+                line = in.readLine();
+            }
+
+        }  catch (IOException e) {
+            Gson gson = new Gson();
+            Reader reader = Files.newBufferedReader(Paths.get("app/src/main/resources/quotes.json"));
+            BufferedReader readJson = new BufferedReader(reader);
+            Quote[] result = gson.fromJson(readJson, Quote[].class);
+            int max = result.length;
+            int min = 1;
+            int randomLine = (int)Math.floor(Math.random()*(max-min+1)+min);
+            System.out.println(result[randomLine].toString());
+        }
 
     }
 
